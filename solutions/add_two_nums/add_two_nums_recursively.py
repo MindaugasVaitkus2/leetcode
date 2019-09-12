d6af4983da0831feef8689c7c7c7f16710eabe3d
TeluguGameboy <telugu.gameboy@gmail.com>
2019-09-11 23:10:45 -0700

    Another example.

--- solutions/add_two_nums/add_two_nums_recursively.py
+++ solutions/add_two_nums/add_two_nums_recursively.py
@@ -11,6 +11,7 @@ class Solution(object):
 
     # Need to pass in carry to helper.
     def addTwoNumbersHelper(self, l1, l2, carry):
+
         # Main base case.
         if l1 is None and l2 is None:
             if carry == 1:
@@ -31,15 +32,18 @@ class Solution(object):
         if l1 is None:
             l2.val = currentVal
             l2.next = self.addTwoNumbersHelper(l1, l2.next, currentCarry)
+            print(linked_list_str(l2))
             return l2
         if l2 is None:
             l1.val = currentVal
             l1.next = self.addTwoNumbersHelper(l1.next, l2, currentCarry)
+            print(linked_list_str(l1))
             return l1
 
         # Final case.
         l1.val = currentVal
         l1.next = self.addTwoNumbersHelper(l1.next, l2.next, currentCarry)
+        print(linked_list_str(l1))
         return l1
 
 # Recursively print linked list
@@ -48,23 +52,46 @@ def linked_list_str(l):
         return ''
     return str(l.val) + '->' + linked_list_str(l.next)
 
+# print
+
+# # Make first linked list.
+# l1 = ListNode(2)
+# l1.next = ListNode(4)
+# l1.next.next = ListNode(3)
+# print(linked_list_str(l1))
+
+# # Make second linked list.
+# l2 = ListNode(5)
+# l2.next = ListNode(6)
+# l2.next.next = ListNode(4)
+# print(linked_list_str(l2))
+
+# print ''
+
+# # Add linked lists.
+# s = Solution()
+# l3 = s.addTwoNumbers(l1, l2)
+
+# print ''
+# print(linked_list_str(l3))
+
 print
 
 # Make first linked list.
-l1 = ListNode(2)
-l1.next = ListNode(4)
-l1.next.next = ListNode(3)
+l1 = ListNode(1)
 print(linked_list_str(l1))
 
 # Make second linked list.
-l2 = ListNode(5)
-l2.next = ListNode(6)
-l2.next.next = ListNode(4)
+l2 = ListNode(9)
+l2.next = ListNode(9)
+l2.next.next = ListNode(9)
 print(linked_list_str(l2))
+
+print ''
 
 # Add linked lists.
 s = Solution()
 l3 = s.addTwoNumbers(l1, l2)
-print(linked_list_str(l3))
 
-print
+print ''
+print(linked_list_str(l3))
